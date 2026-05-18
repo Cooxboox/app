a8807209ed8019c248aa7c33dbdd4d7fd9c59052
Francis Pion <francispion@hotmail.com>
2026-05-17 23:06:21 -0400

    Initialized Web application. (#1)

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
+import HomeView from "./views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
+      name: "Home",
       component: HomeView,
     },
+    // NotFound
     {
-      path: "/about",
-      name: "about",
+      name: "NotFound",
+      path: "/:pathMatch(.*)*",
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (NotFound.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("./views/NotFound.vue"),
+      meta: { isPublic: true },
     },
   ],
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import vueDevTools from "vite-plugin-vue-devtools";
+import packageJson from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  define: {
+    "import.meta.env.VITE_APP_VERSION": JSON.stringify(packageJson.version),
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
